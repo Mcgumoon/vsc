bf5872ca489db56e95e6ab396095f37d9b3a87b4
Ashley Davila <ashleyldavila@gmail.com>
2025-01-25 14:23:42 -0500

    Add Id4Max and Id4MaxTest files

---
+++ Id4Max/Id.cs
@@ -0,0 +1,12 @@
+﻿namespace Id4Max;
+using System;
+
+public class Id
+{
+    public static double Max(double number1, double number2)
+    {
+        double maxNum = Math.Max(number1, number2);
+        Console.WriteLine("The max number between " + number1 + " and " + number2 + " is " + maxNum);
+        return maxNum;
+    }
+}
---
+++ Id4MaxTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Id4Max; // Correctly include the namespace where the Id class is defined
+
+namespace Id4MaxTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        double x = 6;
+        double y = 3;
+        double expected = 6;
+        double actual = Id.Max(x, y); // Correct reference to Id.Max
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2(){
+        double x = 237;
+        double y = 102;
+        double expected = 237;
+        double actual = Id.Max(x, y); // Correct reference to Id.Max
+        Assert.Equal(expected, actual);
+    }
+}
